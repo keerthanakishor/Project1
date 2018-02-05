b2be12a469c4e31dc2f457bcc9b55a487ee6e814
Keerthana <mca003@mcalab-debian.mcalab.edu>
2018-02-05 14:18:20 +0530

    greatest.sh

---
+++ greatest.sh
@@ -0,0 +1,15 @@
+echo "Enter three Integers:"
+read a;
+read  b;
+read c;
+
+if [ $a -gt $b -a $a -gt $c ]
+then
+	echo "$a is Greatest"
+elif [ $b -gt $c -a $b -gt $a ]
+then
+	echo "$b is Greatest"
+else
+	echo "$c is Greatest!"
+fi
+
